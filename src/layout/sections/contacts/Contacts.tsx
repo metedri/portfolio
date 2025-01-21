273cba5bfee9f7cb386d24e51c681a5a8259e4b8
metedri <mitedria@gmail.com>
2025-01-21 16:52:27 +0300

    Refactoring FormMessage and Modal Window

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,49 +1,52 @@
-import React, { useState } from "react";
-import { SectionTitle } from "../../../components/SectionTitle";
-import { Icon } from "../../../components/icon/Icon";
-import { Container } from "../../../components/Container";
-import { FlexWrapper } from "../../../components/FlexWrapper";
-import { StyledButton } from "../../../components/StyledButton";
-import { FormMessage } from "./formMessage/FormMessage";
-import { S } from "./Contacts_Styles";
-import {socials} from "../../../data/socials";
-import { useTranslation } from "react-i18next";
+import React, { useState } from 'react'
+import { SectionTitle } from '../../../components/SectionTitle'
+import { Icon } from '../../../components/icon/Icon'
+import { Container } from '../../../components/Container'
+import { FlexWrapper } from '../../../components/FlexWrapper'
+import { StyledButton } from '../../../components/StyledButton'
+import { FormMessage } from './formMessage/FormMessage'
+import { S } from './Contacts_Styles'
+import { socials } from '../../../data/socials'
+import { useTranslation } from 'react-i18next'
 
 export const Contacts: React.FC = () => {
-    const { t } = useTranslation();
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const { t } = useTranslation()
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const openModal = () => {
-    setIsModalOpen(true);
-  };
+    setIsModalOpen(true)
+  }
 
   const closeModal = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
 
-    return (
-        <S.Contacts id="contacts">
-            <Container>
-                <FlexWrapper direction="column" align="center">
-                    <SectionTitle>{t('contacts.title')}</SectionTitle>
-                    <S.ContactDiscription>{t('contacts.description')}</S.ContactDiscription>
-                    <FormMessage isOpen={isModalOpen} onClose={closeModal}>
-                    </FormMessage>
-                    <StyledButton onClick={openModal}>{t('contacts.formCallBtn')}</StyledButton>
-                    <S.SocialList>
-                        {socials.map((item, index) => {
-                            return(
-                            <S.SocialItem key={index}>
-                                <S.SocialIconLink target="_blank" href={item.href}>
-                                    <Icon height={"40"} width={"40"} iconID={item.iconID} />
-                                </S.SocialIconLink>
-                            </S.SocialItem>
-                        )})}
-                    </S.SocialList>
-                    <S.ContactWithMe>{t('contacts.smallText')}<br />LinkedIn, Telegram, Instagram, VK</S.ContactWithMe>
-                </FlexWrapper>
-            </Container>
-        </S.Contacts>
-    )
+  return (
+    <S.Contacts id="contacts">
+      <Container>
+        <FlexWrapper direction="column" align="center">
+          <SectionTitle>{t('contacts.title')}</SectionTitle>
+          <S.ContactDiscription>{t('contacts.description')}</S.ContactDiscription>
+          {isModalOpen && <FormMessage onClose={closeModal}></FormMessage>}
+          <StyledButton onClick={openModal}>{t('contacts.formCallBtn')}</StyledButton>
+          <S.SocialList>
+            {socials.map((item, index) => {
+              return (
+                <S.SocialItem key={index}>
+                  <S.SocialIconLink target="_blank" href={item.href}>
+                    <Icon height={'40'} width={'40'} iconID={item.iconID} />
+                  </S.SocialIconLink>
+                </S.SocialItem>
+              )
+            })}
+          </S.SocialList>
+          <S.ContactWithMe>
+            {t('contacts.smallText')}
+            <br />
+            LinkedIn, Telegram, Instagram, VK
+          </S.ContactWithMe>
+        </FlexWrapper>
+      </Container>
+    </S.Contacts>
+  )
 }
-
--- src/layout/sections/contacts/formMessage/FormMessage.tsx
+++ src/layout/sections/contacts/formMessage/FormMessage.tsx
@@ -1,61 +1,50 @@
-import React, { ElementRef, useRef } from "react";
-import ReactDOM from "react-dom";
-import { StyledButton } from "../../../../components/StyledButton";
-import {S} from "../Contacts_Styles";
-import emailjs from '@emailjs/browser';
-import { useTranslation } from "react-i18next";
+import React, { ElementRef, useRef } from 'react'
+import ReactDOM from 'react-dom'
+import { StyledButton } from '../../../../components/StyledButton'
+import { S } from '../Contacts_Styles'
+import emailjs from '@emailjs/browser'
+import { useTranslation } from 'react-i18next'
 
-interface ModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    children?: React.ReactNode;
+type Props = {
+  onClose: () => void
 }
 
+export const FormMessage = ({ onClose }: Props) => {
+  const { t } = useTranslation()
+  const form = useRef<ElementRef<'form'>>(null)
 
-export const FormMessage: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-    const { t } = useTranslation();
-    
-    const form = useRef<ElementRef<'form'>>(null);
-    
-    const sendEmail = (e: any) => {
-        e.preventDefault();
-        if(!form.current)return
-        emailjs
-        .sendForm('service_05qbr7j', 'template_i6xn0yv', form.current, {
-            publicKey: 'Yhakanmy4OnUA9rI4',
-        })
-        .then(
-            () => {
-            console.log('SUCCESS!');
-            },
-            (error) => {
-            console.log('FAILED...', error.text);
-        });
-        e.target.reset();
-        onClose();
-    };
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!form.current) return
+    emailjs
+      .sendForm('service_05qbr7j', 'template_i6xn0yv', form.current, {
+        publicKey: 'Yhakanmy4OnUA9rI4',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!')
+        },
+        error => {
+          console.log('FAILED...', error.text)
+        }
+      )
+    e.currentTarget.reset()
+    onClose()
+  }
 
-    if (!isOpen) return null;
-
-    const modalRoot = document.getElementById('modal-root');
-    if (!modalRoot) return null;
-
-    return ReactDOM.createPortal(
-        <S.ModalOverlay onClick={onClose}>
-        <S.Modal onClick={onClose}>
-            <S.CloseButton onClick={onClose}>X</S.CloseButton>
-            <S.Form onClick={(e) => e.stopPropagation()} ref={form} onSubmit={sendEmail}>
-                <S.Field required placeholder={"name"} name={"from_name"}/>
-                <S.Field required placeholder={"email"} name={"email"}/>
-                <S.Field required placeholder={"subject"} name={"subject"}/>
-                <S.Field required placeholder={"message"}as={"textarea"} name={"message"}/>
-                <StyledButton type={"submit"}>{t('contacts.sendBtn')}</StyledButton>
-            </S.Form>
-        </S.Modal>
-        </S.ModalOverlay>,
-        modalRoot
-    )
+  return ReactDOM.createPortal(
+    <S.ModalOverlay onClick={onClose}>
+      <S.Modal onClick={onClose}>
+        <S.CloseButton onClick={onClose}>X</S.CloseButton>
+        <S.Form onClick={e => e.stopPropagation()} ref={form} onSubmit={sendEmail}>
+          <S.Field required placeholder={'name'} name={'from_name'} />
+          <S.Field required placeholder={'email'} name={'email'} />
+          <S.Field required placeholder={'subject'} name={'subject'} />
+          <S.Field required placeholder={'message'} as={'textarea'} name={'message'} />
+          <StyledButton type={'submit'}>{t('contacts.sendBtn')}</StyledButton>
+        </S.Form>
+      </S.Modal>
+    </S.ModalOverlay>,
+    document.body
+  )
 }
-
-
-
