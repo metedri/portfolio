d4533c523c6364dfbdfac7cb8d65339e3c6e8e92
metedri <mitedria@gmail.com>
2025-03-14 22:04:56 +0300

    updated link to the project

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -1,6 +1,4 @@
 import progect1 from '../assets/images/progect_1.webp'
-import progect2 from '../assets/images/progect_2.webp'
-import progect3 from '../assets/images/progect_3.webp'
 
 export type PortfolioDataType = {
   title: string
@@ -12,16 +10,6 @@ export const portfolioData: PortfolioDataType[] = [
   {
     title: 'Pictorial',
     src: progect1,
-    link: 'https://www.google.com/',
+    link: 'https://pictorial.work',
   },
-  //   {
-  //     title: 'progectName2',
-  //     src: progect2,
-  //     link: 'https://www.google.com/',
-  //   },
-  //   {
-  //     title: 'progectName3',
-  //     src: progect3,
-  //     link: 'https://www.google.com/',
-  //   },
 ]
